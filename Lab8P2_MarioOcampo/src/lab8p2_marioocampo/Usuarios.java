4385daf67104ae696a3492e391517983107caaaf
mariocampoo <marioandreom69@gmail.com>
2023-12-01 14:36:08 -0600

    Lab8P2_MarioOcampo

---
+++ Lab8P2_MarioOcampo/src/lab8p2_marioocampo/Usuarios.java
@@ -0,0 +1,36 @@
+package lab8p2_marioocampo;
+public class Usuarios{
+    private String usuario;
+    private String password;
+
+    public Usuarios(String usuario, String password) {
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuarios" + "\n"
+                + " Usuario = " + usuario + "\n"
+                + " Password = " + password;
+    }
+    
+    
+    
+}
